088590c3e2e36669cf2271967171ecf686ba376a
yqs <1064415703@qq.com>
2021-07-28 23:03:35 +0800

    提交算法

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var msgs = make(chan int)
+var done = make(chan struct{})
 
 func main() {
-	fmt.Println("test...")
+	go send(msgs)
+	go receive(msgs)
+
+	time.Sleep(5 * time.Second)
+}
+
+func send(ch chan int) {
+	for {
+		<-time.After(1 * time.Second)
+		select {
+		case <-done:
+			fmt.Println("done")
+			return
+		case ch <- time.Now().Second():
+		}
+	}
+}
+
+func receive(ch chan int) {
+	<-time.After(1 * time.Second)
+	msg := <-ch
+	fmt.Println(msg)
+	// 关闭前通知结束
+	done <- struct{}{}
+	close(ch)
 }
